f4ddd5c972bc42a7734345ebe8cfe209a993c7df
JonathanC1021 <38872639+JonathanC1021@users.noreply.github.com>
2018-12-05 21:00:18 -0800

    Update CIS7-Project-1.cpp

--- CIS7-Project-1.cpp
+++ CIS7-Project-1.cpp
@@ -1,10 +1,13 @@
-// Jonathan Carreon
+// Jonathan Carreon (Encryption)
+// Guadalupe Chavez (Decrption)
 // CIS7
-// Project: Case Project 3 (Encryption)
+// Project: Case Project 3 
 // Team Name: Basic CPU
 #include <iostream>
 using namespace std;
 
+// This function is used to generate the key in a cycle way
+// until its length is not equal to the plain text.
 string generateKey(string text, string key)
 {
   int v = text.size();
@@ -20,6 +23,8 @@ string generateKey(string text, string key)
   return key; 
 }
 
+// This function is used to return the generated encrypted
+// plain text with the help of the key.
 string cipherText(string text, string key)
 {
   string cipher_text;
@@ -33,6 +38,23 @@ string cipherText(string text, string key)
     cipher_text.push_back(v);
   }
   return cipher_text;
+}
+
+// This function will decrypt the encrypted text and return 
+// the plain text.
+string decryptedText(string cipher_text, string key)
+{
+  string decrypt_text;
+
+  for (int i = 0; i < cipher_text.size(); i++)
+  {
+    int v = (cipher_text[i] - key[i] + 26) %26;
+
+    v += 'A';
+
+    decrypt_text.push_back(v);
+  }
+  return decrypt_text;
 }
 
 // Use 'WEWILLTRY' as Plaintext
@@ -51,8 +73,8 @@ int main()
   string cipher_text = cipherText(text, key);
 
   cout << "Ciphertext: " << cipher_text << "\n";
+  
+  cout << "Decrypted Text: " << decryptedText(cipher_text, key);
 
   return 0;
 }
-
-***************************************************************
